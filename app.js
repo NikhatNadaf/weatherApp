ec621d7d37bf40f83e3b6ce8282337b8305e3701
Nikhat Nadaf <nikhatnadaf1128@gmail.com>
2024-05-22 12:10:53 +0530

    add

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ async function checkWeather(city){
     const api_key="ecb5b4602bd886245cc0c402eef83919";
     const url= `https://api.openweathermap.org/data/2.5/weather?id=${city}&appid=${api_key}`;
     const weather_data= await fetch(`${url}`).then(response => response.json());
-    console.log(weather_data)
+    console.log(weather_data);
 }
 btn.addEventListener("click",()=>{
     checkWeather(inputBox.value);
