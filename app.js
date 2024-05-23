a9461217d1e03d3b1939ac97d12e37432d37c6ce
Nikhat Nadaf <nikhatnadaf1128@gmail.com>
2024-05-23 13:00:06 +0530

    complete all

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const inputBox=document.querySelector(".input-box");
 const btn=document.getElementById("btn");
 const weather_img=document.querySelector(".weather-img");
-const temp=document.querySelector(".temp");
+const tempe=document.querySelector(".temp");
 const desc=document.querySelector(".desc");
 const humidity=document.getElementById("humidity");
 const wind_speed=document.getElementById("wind-speed");
@@ -9,9 +9,42 @@ const wind_speed=document.getElementById("wind-speed");
 
 async function checkWeather(city){
     const api_key="ecb5b4602bd886245cc0c402eef83919";
-    const url= `https://api.openweathermap.org/data/2.5/weather?id=${city}&appid=${api_key}`;
+    const url= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
     const weather_data= await fetch(`${url}`).then(response => response.json());
-    console.log(weather_data);
+
+    //if user enters wrong name
+    if(weather_data.cod===`404`)
+        {
+            weather_img.src="./assest/pageNotFound.jpg";
+            alert('Please enter correct name');
+            return;
+        }
+   tempe.innerHTML=`${Math.round(weather_data.main.temp - 273.15)}°C`; //kelvin to celsius =(kelvin-273.15)
+   desc.innerHTML=`${weather_data.weather[0].description}`
+   humidity.innerHTML=`${weather_data.main.humidity}%`;
+   wind_speed.innerHTML=`${weather_data.wind.speed}Km/H`;
+
+   //tp put images
+   switch(weather_data.weather[0].main)
+   {
+    case "Clouds":
+        weather_img.src="./assest/cloud.png";
+        break;
+    
+    case "Clear":
+        weather_img.src="./assest/clear.png";
+        break;
+    case "Rain":
+        weather_img.src="./assest/rain.png";
+        break;
+    case "Mist":
+        weather_img.src="./assest/mist.png";
+        break;
+    case "Snow":
+        weather_img.src="./assest/snow.png";
+        break;
+   }
+   console.log(weather_data);
 }
 btn.addEventListener("click",()=>{
     checkWeather(inputBox.value);
--- readme.md
+++ readme.md
@@ -7,3 +7,45 @@ api_url=https://api.openweathermap.org/data/2.5/weather?id={city id}&appid={API 
 whenever user enter city name in input area and hit button as search then urlis fetch and return weather information of perticular city.
 
 API_Key:we have use api key which is generated after 2-3 hourse of account creating on website.
+
+values return by api....
+Object
+base
+: 
+"stations"
+clouds
+: 
+{all: 16}
+cod
+: 
+200
+coord
+: 
+{lon: 73.8553, lat: 18.5196}
+dt
+: 
+1716449265
+id
+: 
+1259229
+main
+: 
+{temp: 312.2, feels_like: 311.48, temp_min: 312.2, temp_max: 312.2, pressure: 1002, …}
+name
+: 
+"Pune"
+sys
+: 
+{country: 'IN', sunrise: 1716424125, sunset: 1716471255}
+timezone
+: 
+19800
+visibility
+: 
+10000
+weather
+: 
+[{…}]
+wind
+: 
+{speed: 3.83, deg: etc.
